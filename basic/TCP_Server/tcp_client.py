7999f09f895a7d86e0629e3a5c5e7b0b949bdd9c
yezinoo <yezinoo.ucsm@gmail.com>
2025-09-30 23:52:08 +0900

    added

---
+++ basic/TCP_Server/tcp_client.py
@@ -0,0 +1,14 @@
+import socket
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect("127.0.0.1",8080)
+print("Connected to server. You can start chatting")
+while True:
+    msg = input("Client: ")
+    client.sendall(msg.encode())
+    data = client.recv(1024)
+    if not data:
+        break
+    print("Server:", data.decode())
+client.close()
+    
