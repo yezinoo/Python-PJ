719bc67aa456d76402720ee6583038bddbfe36fe
yezinoo <yezinoo.ucsm@gmail.com>
2025-09-30 23:32:01 +0900

    folder

---
+++ basic/TCP_Server/tcp_chat_server.py
@@ -0,0 +1,18 @@
+import socket
+
+server_ip = "0.0.0.0"
+server_port = 8080
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((server_ip,server_port))
+server.listen(1)
+print("Server is listening on port 8080...")
+
+conn, addr = server.accept()
+print(f"Connected by {addr}")
+
+while true:
+    data = conn.recv(1024)
+    if not data:
+        break;
+    print("Client",data.decode())
